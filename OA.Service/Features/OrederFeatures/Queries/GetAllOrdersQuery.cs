cbdcb8a92e99fb8e683afa34f1a824b1dd681cb3
AlpeshShivankar <shivankar.alpesh@gmail.com>
2024-08-27 15:32:34 +0530

    Order features addition

---
+++ OA.Service/Features/OrederFeatures/Queries/GetAllOrdersQuery.cs
@@ -0,0 +1,32 @@
+﻿using ECom.Domain.Entities;
+using ECom.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECom.Service.Features.OrederFeatures.Queries
+{
+    public class GetAllOrderQuery:IRequest<IEnumerable<Order>>
+    {
+        public class GetAllOrderQueryHandler : IRequestHandler<GetAllOrderQuery, IEnumerable<Order>>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetAllOrderQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<IEnumerable<Order>> Handle(GetAllOrderQuery request, CancellationToken cancellationToken)
+            {
+                var orderList = await _context.Orders.ToListAsync();
+                if (orderList == null)
+                {
+                    return null;
+                }
+                return orderList.AsReadOnly();
+            }
+
+        }
+    }
+}
---
+++ OA.Service/Features/OrederFeatures/Queries/GetOrderByIdQuery.cs
@@ -0,0 +1,28 @@
+﻿using ECom.Domain.Entities;
+using ECom.Persistence;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECom.Service.Features.OrederFeatures.Queries
+{
+    public  class GetOrderByIdQuery:IRequest<Order>
+    {
+        public int Id { get; set; }
+        public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, Order>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetOrderByIdQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Order> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+            {
+                var order = _context.Orders.Where(a => a.Id == request.Id).FirstOrDefault();
+                if (order == null) return null;
+                return order;
+            }
+        }
+    }
+}
